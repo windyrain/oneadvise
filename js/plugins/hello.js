ac457ca1bbcf539c9ea70daac93a20c3d6d88c19
封宇 <windyrain1994@163.com>
2019-12-16 23:53:37 +0800

    doc: add comment for hello.js

--- js/plugins/hello.js
+++ js/plugins/hello.js
@@ -1,3 +1,4 @@
+// fix not found requestIdleCallback
 if (!window.requestIdleCallback) {
   window.requestIdleCallback = ((func) => {
     setTimeout(func, 500);
@@ -5,16 +6,20 @@ if (!window.requestIdleCallback) {
 }
 
 requestIdleCallback(function () {
+  // not request when local debug
   if (location.protocol === 'file:') {
     return;
   }
 
+  // get fingerprint2 key
   Fingerprint2.get(function (components) {
     const hash = Fingerprint2.x64hash128(components.map(({ value }) => value).join(''),31);
 
+    // search hash in server
     fetch(`http://www.oneadvise.cn:1337/friends?fingerprint=${hash}`)
       .then(response => response.json())
       .then((result) => {
+        // not found, add a new user
         if (result && result.length === 0) {
           fetch('http://www.oneadvise.cn:1337/friends', {
             method: 'POST',
@@ -26,15 +31,20 @@ requestIdleCallback(function () {
             })
           });
 
+          // welcome new friend
           Chat.preMessages.push(`欢迎你，我的新朋友，我叫封宇，是这个网站的建设者，很高兴认识你！`)
         } else {
+          // if have name is my friend
           if (result && result[0] && result[0].name) {
             let call = '';
             const info = result[0];
             const myAge = 1993;
             const { name, tag, age, sex } = info;
+
+            // show name
             Chat.preMessages.push(`是你吗，${name}${tag ? `，My ${tag}！` : '。'}`);
 
+            // according sex and age to set call
             if (sex === 'male') {
               if (age > myAge) {
                 call = '老弟';
@@ -49,6 +59,7 @@ requestIdleCallback(function () {
               }
             }
 
+            // private setting
             if (tag === 'girlfriend') {
               call = '小宝贝';
               Chat.preMessages.push('亲爱的，谢谢你来支持我的网站，爱你哟！');
@@ -71,6 +82,7 @@ requestIdleCallback(function () {
 
             document.querySelector('.welcome').innerHTML = `欢迎光临，${call}！`;
           } else {
+            // stranger welcome
             Chat.preMessages.push(`欢迎你，我的新朋友，我叫封宇，是这个网站的建设者，很高兴认识你！`);
           }
         }
